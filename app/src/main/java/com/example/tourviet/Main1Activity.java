68918e97cfad88fdfc5406b7d81be5565cd9a8ef
trongtinvd <trongtinvd@gmail.com>
2019-12-03 15:37:31 +0700

    phân tách main2 activity thành 2 activity riêng lẻ cho tiện quản lý

--- app/src/main/java/com/example/tourviet/Main1Activity.java
+++ app/src/main/java/com/example/tourviet/Main1Activity.java
@@ -1,13 +1,13 @@
 package com.example.tourviet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,8 +16,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Main1Activity extends AppCompatActivity {
-    EditText editAddressdk,editPassdk,editEmaildk,editHo_tendk,editSDTdk,editDatedk,editPhaidk;
-    Button buttonDangKi,buttonHuy;
+    EditText editAddressdk, editPassdk, editEmaildk, editHo_tendk, editSDTdk, editDatedk, editPhaidk;
+    Button buttonDangKi, buttonHuy;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +32,9 @@ public class Main1Activity extends AppCompatActivity {
         buttonDangKi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(editAddressdk.getText().length()!=0 && editPassdk.getText().length()!=0 && editEmaildk.getText().length()!=0 && editSDTdk.getText().length()!=0 && editHo_tendk.getText().length()!=0 && editDatedk.getText().length()!=0 && editPhaidk.getText().length()!=0) {
+                if (editAddressdk.getText().length() != 0 && editPassdk.getText().length() != 0 && editEmaildk.getText().length() != 0 && editSDTdk.getText().length() != 0 && editHo_tendk.getText().length() != 0 && editDatedk.getText().length() != 0 && editPhaidk.getText().length() != 0) {
 
-                    User user=new User(
+                    User user = new User(
 
                             editPassdk.getText().toString().trim(),
                             editHo_tendk.getText().toString().trim(),
@@ -45,27 +46,24 @@ public class Main1Activity extends AppCompatActivity {
                     );
                     sendNetworkRequest(user);
 
-                }
-                else
-                    Toast.makeText(Main1Activity.this,"mời các bạn nhập đủ thông tin",Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(Main1Activity.this, "mời các bạn nhập đủ thông tin", Toast.LENGTH_SHORT).show();
             }
-            private void sendNetworkRequest(User user)
-            {
-                Retrofit builder= new Retrofit.Builder()
+
+            private void sendNetworkRequest(User user) {
+                Retrofit builder = new Retrofit.Builder()
                         .baseUrl("http://35.197.153.192:3000")
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
 
-                userClient client =builder.create((userClient.class));
+                userClient client = builder.create((userClient.class));
                 Call<User> call = client.CreateAccount(user);
                 call.enqueue(new Callback<User>() {
                     @Override
                     public void onResponse(Call<User> call, Response<User> response) {
                         if (!response.isSuccessful()) {
                             Toast.makeText(Main1Activity.this, "lỗi lấy thông tin từ server.", Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                        else {
+                        } else {
                             Toast.makeText(Main1Activity.this, "Đăng ký thành công với Id: " + response.body().getId(), Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(Main1Activity.this, MainActivity.class);
                             startActivity(intent);
@@ -74,33 +72,33 @@ public class Main1Activity extends AppCompatActivity {
 
                     @Override
                     public void onFailure(Call<User> call, Throwable t) {
-                        Toast.makeText(Main1Activity.this,"Đăng ký chưa thành công",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Main1Activity.this, "Đăng ký chưa thành công", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
-        }) ;
+        });
         buttonHuy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Main1Activity.this,MainActivity.class);
+                Intent intent = new Intent(Main1Activity.this, MainActivity.class);
                 startActivity(intent);
 
             }
         });
 
 
-
     }
+
     private void Anhxa() {
-        editAddressdk = (EditText)findViewById(R.id.dangki_Address);
-        editPassdk = (EditText)findViewById(R.id.dangki_Pass);
-       editEmaildk = (EditText)findViewById(R.id.dangki_email);
-        editHo_tendk = (EditText)findViewById(R.id.dangki_Ho_Ten);
-        editSDTdk = (EditText)findViewById(R.id.dangki_SDT);
-        editDatedk = (EditText)findViewById(R.id.dangki_Date) ;
-        editPhaidk = (EditText)findViewById(R.id.dangki_Phai) ;
-        buttonDangKi = (Button)findViewById(R.id.buttonDangKi_dangki);
-        buttonHuy = (Button)findViewById(R.id.buttonDangKi_huy);
+        editAddressdk = findViewById(R.id.dangki_Address);
+        editPassdk = findViewById(R.id.dangki_Pass);
+        editEmaildk = findViewById(R.id.dangki_email);
+        editHo_tendk = findViewById(R.id.dangki_Ho_Ten);
+        editSDTdk = findViewById(R.id.dangki_SDT);
+        editDatedk = findViewById(R.id.dangki_Date);
+        editPhaidk = findViewById(R.id.dangki_Phai);
+        buttonDangKi = findViewById(R.id.buttonDangKi_dangki);
+        buttonHuy = findViewById(R.id.buttonDangKi_huy);
 //        button đăng nhập bằng google và facebook
 //         .........................
 
--- app/src/main/java/com/example/tourviet/Main2Activity.java
+++ app/src/main/java/com/example/tourviet/Main2Activity.java
@@ -1,38 +1,26 @@
 package com.example.tourviet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.facebook.login.LoginManager;
-import java.util.ArrayList;
-import java.util.List;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Main2Activity extends AppCompatActivity {
 
-    TourAdapter tourAdapter;
-    ListView tourListView;
-    Button Backbutton,creattour;
-    TextView Name;
-    List<TourItem> tourData = new ArrayList<TourItem>();
+    Button creatTourBtn;
+    Button userProfileBtn;
+    Button viewAvailableTourBtn;
 
-    String token,image_url;
+    String token, image_url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Anhxa();
-        controlbutton();
+        setOnClickEvent();
 
         // bắt token từ đăng nhập
         Intent intent = getIntent();
@@ -42,156 +30,57 @@ public class Main2Activity extends AppCompatActivity {
             token = bundle.getString("Key_1");
             image_url = bundle.getString("Key_3");
         }
-        loaddata();
-
     }
-    private String convert (String string)
-    {
-        String kq;
-        StringBuilder sb = new StringBuilder(string);
-        for (int index = 0; index < sb.length(); index++) {
-            char c = sb.charAt(index);
-            if (Character.isLowerCase(c)) {
-                sb.setCharAt(index, Character.toUpperCase(c));
-//            } else {
-//                sb.setCharAt(index, Character.toLowerCase(c));
-//            }
-            }
-        }
-        return  kq = sb.toString();
-    }
-    private void loaddata()
-    {
-        tourData.add(new TourItem(99, 0, "Đi hạ long (sample)", 100000, 200000, "21/10/1999", "27/2/2014", 0, 0, false, "https://tour.dulichvietnam.com.vn/uploads/tour/1554713265_tour-ha-long-3.jpg"));
-        tourData.add(new TourItem(66, 0, "Đi đà lạc (sample)", 700000, 900000, "14/5/216", "8/2/374", 0, 0, false, "https://cdn3.ivivu.com/2013/09/khu-nghi-duong-terracotta-da-lat-1-800x450.jpg"));
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://35.197.153.192:3000")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        userClient client = retrofit.create((userClient.class));
-
-        Call<User_infor> call = client.GetUserInfor(token);
-        call.enqueue(new Callback<User_infor>() {
-            @Override
-            public void onResponse(retrofit2.Call<User_infor> call, Response<User_infor> response) {
-                if (!response.isSuccessful()) {
-                    Toast.makeText(Main2Activity.this, "lỗi lấy thông tin từ server.", Toast.LENGTH_LONG).show();
-
-                    return;
-                }
-                // chuyển tên thành chữ in hoa
-                StringBuilder sb = new StringBuilder(response.body().getFullName());
-                for (int index = 0; index < sb.length(); index++) {
-                    char c = sb.charAt(index);
-                    if (Character.isLowerCase(c)) {
-                        sb.setCharAt(index, Character.toUpperCase(c));
-                    } else {
-                        sb.setCharAt(index, Character.toLowerCase(c));
-                    }
-                }
-                //......................................................
-
-                Name.setText(convert(response.body().getFullName()));
-
-            }
-
-            @Override
-            public void onFailure(retrofit2.Call<User_infor> call, Throwable t) {
-                Toast.makeText(Main2Activity.this, "lỗi lấy thông tin từ server.", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-        Call<TourListGet> call1 = jsonPlaceHolderApi.getTourList(token);
-        call1.enqueue(new Callback<TourListGet>() {
-            @Override
-            public void onResponse(Call<TourListGet> call, Response<TourListGet> response) {
-                if (!response.isSuccessful()) {
-                    Toast.makeText(Main2Activity.this, "lỗi lấy thông tin từ server.", Toast.LENGTH_LONG).show();
-
-                    return;
-                }
-
-                TourListGet tours = response.body();
-                tourData.addAll(tours.getTours());
-                tourAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(Call<TourListGet> call, Throwable t) {
-                Toast.makeText(Main2Activity.this, "lỗi lấy thông tin từ server.", Toast.LENGTH_LONG).show();
-            }
-        });
-
-
-        tourAdapter = new TourAdapter(this, tourData);
-        tourListView = findViewById(R.id.main2_tourListView);
-        tourListView.setAdapter(tourAdapter);
-        tourListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(position==position)
-                {
-                    Long ID= tourData.get(position).getId();
-                    Intent intent = new Intent(Main2Activity.this,Main3Activity.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putLong("Key_2", ID);
-                    intent.putExtras(bundle);
-                    bundle.putString("Key_1", token);
-                    intent.putExtras(bundle);
-                    bundle.putString("Key_3", image_url);
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-
-                }
-            }
-        });
-    }
-    private void controlbutton (){
-
-        Backbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Main2Activity.this,MainActivity.class);
-                startActivity(intent);
-                LoginManager.getInstance().logOut();
-
-            }
-        });
-        creattour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Main2Activity.this,Main4Activity_Creat.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("Key_1", token);
-                intent.putExtras(bundle);
-                bundle.putString("Key_3", image_url);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
-
-
-        Name.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Main2Activity.this,Main2Activity_user.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("Key_1", token);
-                intent.putExtras(bundle);
-                bundle.putString("Key_3", image_url);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
-
-    }
     private void Anhxa() {
+        userProfileBtn = findViewById(R.id.main2_viewUserProfileBtn);
+        creatTourBtn = findViewById(R.id.main2_createTourBtn);
+        viewAvailableTourBtn = findViewById(R.id.main2_viewAvailableTourBtn);
+    }
 
-        Backbutton = (Button)findViewById(R.id.backge);
-        creattour = (Button)findViewById(R.id.addtour);
-        Name = (TextView)findViewById(R.id.nameUer);
+
+    private void setOnClickEvent() {
+
+        creatTourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, Main4Activity_Creat.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("Key_1", token);
+                intent.putExtras(bundle);
+                bundle.putString("Key_3", image_url);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+
+        userProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, Main2Activity_user.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("Key_1", token);
+                intent.putExtras(bundle);
+                bundle.putString("Key_3", image_url);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+
+        viewAvailableTourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, AvailableTourActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("Key_1", token);
+                intent.putExtras(bundle);
+                bundle.putString("Key_3", image_url);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
 
     }
 
